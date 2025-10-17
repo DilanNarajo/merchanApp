278665a19edf8e30db5942d9ad91059dd8c522c6
DilanNaranjo <dilan.naranjo1999@gmail.com>
2025-10-16 20:53:37 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'merchanApp',
+  webDir: 'www'
+};
+
+export default config;
